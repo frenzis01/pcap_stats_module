83908c63e0315270b45cc82d5cfc4a02888473d7
frenzis01 <francesco.lorenzoni12042@gmail.com>
2022-06-10 23:27:11 +0200

    refactoring code

--- test.py
+++ test.py
@@ -11,14 +11,16 @@ def find_pkt(mac_src, ip_src, ip_dst, pcap):
       if(pkt.src == mac_src and pkt[IP].src == ip_src and pkt[IP].dst == ip_dst):
          return pkt
 
-def avg_delta_echopkts(pcap):
+def avg_delta_pkts(pcap, echo):
    time_sum = 0.0
    t_start = t_end = 0.0
    n = 0
    pkt_prev = pcap[0]
    for pkt in pcap:
       # consider only pkts B->A
-      if (pkt.dst != mac_A and pkt.dst != mac_RasPi):
+      if (echo == True and pkt.dst != mac_A and pkt.dst != mac_RasPi):
+         continue
+      if (echo == False and (pkt.dst == mac_A or pkt.dst == mac_RasPi)):
          continue
       if (n != 0):
          time_sum += pkt.time - pkt_prev.time
@@ -46,14 +48,14 @@ def avg_rtt(pcap):
    return rtt_sum/n
 
 
-def read_pcaps():
+def read_pcaps(path):
    import os
    import sys
    import fnmatch
 
    pcap_files = []
    pcap_paths = []
-   for path,dirs,files in os.walk('.'):
+   for path,dirs,files in os.walk(path):
       for f in fnmatch.filter(files,'*.pcap'):
          fullname = os.path.abspath(os.path.join(path,f))
          pcap_paths.append(fullname)
@@ -72,24 +74,54 @@ def read_pcaps():
 import json
 import decimal
 
+def get_paths(path):
+   import os
+   import sys
+   import fnmatch
+   pcap_paths = []
+   for path,dirs,files in os.walk(path):
+      for f in fnmatch.filter(files,'*.pcap'):
+         fullname = os.path.abspath(os.path.join(path,f))
+         pcap_paths.append(fullname)
+   
+   pcap_paths.sort()
+   return pcap_paths
+
+def read_pcap(path):
+   print(path, " Reading...")
+   ret = rdpcap(path)
+   sys.stdout.write("\033[F")
+   sys.stdout.write("\033[K")
+   print(path[path.rfind("/",0,path.rfind("/"))+1:], " DONE")
+   ret.listname = path[path.rfind("/",0,path.rfind("/"))+1:]
+   return ret
+
 class pcap_stats:
-   def __init__(self, name, avg_time, pps):
+   def __init__(self, name, avg_time, ppsB, ppsA, rtt):
       self.name = name
       self.avg_time = avg_time
-      self.pps = pps
+      self.ppsB = ppsB
+      self.ppsA = ppsA
+      self.rtt = rtt
    
    def toJSON(self):
       return self.__dict__
       # return json.dumps(self, default=lambda o: o.__dict__, sort_keys=False, indent = 4)
 
-def write_pcaps_stats(pcap_files, json_path):
+def write_pcaps_stats(pcap_paths, json_path):
    stats  = []
-   for pcap in pcap_files:
-      x = (avg_delta_echopkts(pcap))
-      stats.append(pcap_stats(pcap.listname,str(x[0]),str(x[1])))
+   for path in pcap_paths:
+      pcap = read_pcap(path)
+      x = (avg_delta_pkts(pcap,True))
+      y = (avg_delta_pkts(pcap,False))
+      rtt = avg_rtt(pcap)
+      stats.append(pcap_stats(pcap.listname,str(x[0]),str(x[1]),str(y[1]),rtt))
       print(stats[-1].toJSON())
    print(json.dumps(stats,default=lambda o: o.toJSON(), indent=4))
    f = open(json_path, "w")
    f.write(json.dumps(stats,default=lambda o: o.toJSON(), indent=4))
    f.close()
    return stats
+
+def test():
+   write_pcaps_stats(get_paths(".."), "complete_stats_.json")
